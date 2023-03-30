d5b2c2e00cb8acf9377275eea590de9996e2c196
Renato Figueiredo <rjofig@gmail.com>
2023-03-30 10:35:55 -0400

    Update simple_sequence.R

--- examples/simple_sequence.R
+++ examples/simple_sequence.R
@@ -2,6 +2,9 @@
 # F1 computes the product of each element of two vectors stored as CSV files in S3_A
 # F2 computes the square of each element of a vector stored as CSV file in S3_A
 
+# The user will access FaaSr functions as a library
+library("FaaSr")
+        
 F1 <- function(faasr) {
   # print(“Hello from inside function F1”)
   args <- faasr_get_user_function_args(faasr)
@@ -10,7 +13,7 @@ F1 <- function(faasr) {
   faasr_get_file(faasr, “S3_A”, “folder”, args$input1, “local”, “input1.csv”)
   faasr_get_file(FaaSr, “S3_A”, “folder”, args$input2, “local”, “input2.csv”)
   # need to implement output <- input1 * input2
-  faasr_put_file(faasr, “S3_A”, “folder”, args$output, “local”, “output.csv”)
+  faasr_put_file(faasr, “S3_A”, “local”, “output.csv”, “folder”, args$output)
   faasr_log(faasr, “User function F1 finished”)
 }	
 
@@ -22,6 +25,6 @@ F2 <- function(faasr) {
   # expects input1, input2: input file names; output: 
   faasr_get_file(faasr, “S3_A”, “folder”, args$input1, “local”, “input.csv”)
   # need to implement output <- input * input
-  faasr_put_file(faasr, “S3_A”, “folder”, args$output, “local”, “output.csv”)
+  faasr_put_file(faasr, “S3_A”, “local”, “output.csv”, “folder”, args$output)
   faasr_log(faasr, “User function F2 finished”)
 }	
