f382e840bc762089459e1be3dd7f58d8370999be
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-03-31 15:07:23 -0400

    Update simple_sequence.R

--- examples/simple_sequence.R
+++ examples/simple_sequence.R
@@ -14,11 +14,11 @@ F1 <- function(faasr) {
   faasr_get_file(FaaSr, “S3_A”, “folder”, args$input2, “local”, “input2.csv”)
   # need to implement output <- input1 * input2
 # if there's a header in each csv file, "header=T"
-  input1 <- read.table("local/input1.csv", header=F)
-  input2 <- read.table("local/input2.csv", header=F)
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
   output <- input1 * input2
   # if there's a header in csv file, "col.names=T"
-  write.table(output, file="local/output.csv", row.names=F, col.names=F)
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)
   faasr_put_file(faasr, “S3_A”, “local”, “output.csv”, “folder”, args$output)
   faasr_log(faasr, “User function F1 finished”)
 }	
@@ -32,10 +32,10 @@ F2 <- function(faasr) {
   faasr_get_file(faasr, “S3_A”, “folder”, args$input1, “local”, “input.csv”)
   # need to implement output <- input * input
   # if there's a header in each csv file, "header=T"
-  input <- read.table("local/input.csv", header=F)
+  input <- read.table("local/input.csv", sep=",", header=F)
   output <- input^2
   # if there's a header in csv file, "col.names=T"
-  write.table(output, file="local/output.csv", row.names=F, col.names=F)   
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)   
   faasr_put_file(faasr, “S3_A”, “local”, “output.csv”, “folder”, args$output)
   faasr_log(faasr, “User function F2 finished”)
 }	
