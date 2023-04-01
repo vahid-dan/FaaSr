68ae083cad525d51ef9a7c29186b8a0bd1983b56
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-04-01 13:59:27 -0400

    Add files via upload

---
+++ base_docker_images/openwhisk/exec.R
@@ -0,0 +1,12 @@
+#!/usr/local/bin/Rscript
+#This will be replaced by library("faasr")
+source("faasr.R")
+#Get all functions from Rscripts in the /action/ directory
+r_files <- list.files(pattern="\\.R$")
+for (file in r_files){
+	source(file)
+}
+#Start Ibmcloud action with faasr_start
+faasr <- commandArgs(TRUE)
+faasr_start(faasr)
+
---
+++ base_docker_images/openwhisk/users/example.R
@@ -0,0 +1,34 @@
+F1 <- function(faasr) {
+  # print(“Hello from inside function F1”)
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F1)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+  # need to implement output <- input1 * input2
+# if there's a header in each csv file, "header=T"
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
+  output <- input1 * input2
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", ,sep=",", row.names=F, col.names=T)
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  #faasr_log(faasr, “User function F1 finished”)
+}	
+
+
+F2 <- function(faasr) {
+  # print(“Hello from inside function Fs”)
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F2)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input.csv")
+  # need to implement output <- input * input
+  # if there's a header in each csv file, "header=T"
+  input <- read.table("local/input.csv", sep=",", header=F)
+  output <- input^2
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=T)   
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  #faasr_log(faasr, “User function F2 finished”)
+}	
