f72b606714107bc17dcd1693beeea6f2e3f98daa
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-04-25 16:19:40 -0400

    Create simple_sequence1.R

---
+++ examples/simple_sequence1.R
@@ -0,0 +1,74 @@
+library("FaaSr")
+        
+F1 <- function(faasr) {
+  # print("Hello from inside function F1")
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F1)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+  # need to implement output <- input1 * input2
+# if there's a header in each csv file, "header=T"
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
+  output <- input1 + input2
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  faasr_log(faasr, "User function F1 finished")
+}	
+
+
+F2 <- function(faasr) {
+  # print("Hello from inside function Fs")
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F2)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+  # need to implement output <- input * input
+  # if there's a header in each csv file, "header=T"
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
+  output <- input2 - input1
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)   
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  faasr_log(faasr, "User function F2 finished")
+}	
+
+F3 <- function(faasr) {
+  # print("Hello from inside function Fs")
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F3)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+  # need to implement output <- input * input
+  # if there's a header in each csv file, "header=T"
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
+  output <- input2 - input1
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)   
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  faasr_log(faasr, "User function F3 finished")
+}	
+
+F4 <- function(faasr) {
+  # print("Hello from inside function Fs")
+  args <- faasr_get_user_function_args(faasr)
+  # return arguments for this function (F4)
+  # expects input1, input2: input file names; output: 
+  faasr_get_file(faasr, "S3_A", "folder", args$input1, "local", "input1.csv")
+  faasr_get_file(faasr, "S3_A", "folder", args$input2, "local", "input2.csv")
+  # need to implement output <- input * input
+  # if there's a header in each csv file, "header=T"
+  input1 <- read.table("local/input1.csv", sep=",", header=F)
+  input2 <- read.table("local/input2.csv", sep=",", header=F)
+  output <- input2 + input1
+  # if there's a header in csv file, "col.names=T"
+  write.table(output, file="local/output.csv", sep=",", row.names=F, col.names=F)   
+  faasr_put_file(faasr, "S3_A", "local", "output.csv", "folder", args$output)
+  faasr_log(faasr, "User function F4 finished")
+}	
